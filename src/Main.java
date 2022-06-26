3ed213121eb930139c92895d86b820e9f512c389
Rohin <kotianra@mail.uc.edu>
2022-06-26 19:09:27 -0400

    Share Init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class
+Main
+{
+    public static void main(String[] args)
+    {
+        int birthMonth = 17;
+        if(birthMonth > 0)
+        {
+            if(birthMonth <=12)
+            {
+                System.out.println("Your birth month is " + birthMonth);
+            }
+            else
+            {
+                System.out.println("Invalid input: " + birthMonth);
+            }
+        }
+        else
+        {
+            System.out.println("Invalid input: " + birthMonth);
+        }
     }
 }
